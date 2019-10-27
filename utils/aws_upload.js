83679a4114119c15b52526906d8d085b23fc40ff
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-26 22:53:03 -0700

    configure aws s3

---
+++ utils/aws_upload.js
@@ -0,0 +1,35 @@
+var aws = require('aws-sdk')
+var multer = require('multer')
+var multerS3 = require('multer-s3')
+const config = require('../config/keys')
+aws.config.update({
+  secretAccessKey: config.awsSecretKey,
+  accessKeyId: config.awsAccessKey,
+  region: config.awsRegion
+})
+var s3 = new aws.S3()
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(new Error('Invalid Mime Type, only JPEG and PNG'), false)
+  }
+}
+
+var upload = multer({
+  fileFilter,
+  storage: multerS3({
+    s3: s3,
+    bucket: 'california-athletes',
+    // acl:'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+module.exports = upload;
