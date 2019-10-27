250ab3a53df72514b0eebc1955c9986505fe04d9
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-26 21:48:11 -0700

    add kyes for dev

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+
+  awsSecretKey: process.env.AWS_SECRET,
+  awsAccessKey: process.env.AWS_ACCESS,
+  awsRegion: process.env.AWS_REGION
+};
