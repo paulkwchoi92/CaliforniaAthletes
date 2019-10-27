f2122c85c697015d5945ef2b233ff84d3b438a13
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-26 21:45:24 -0700

    configuring models

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-const express = require("express")
-const app = express()
+const mongoose = require("mongoose");
+// This creates a new Express server.
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+// Imports created routes.
+const users = require("./routes/api/users");
+const pins = require("./routes/api/pins");
+const boards = require("./routes/api/boards");
+const images = require("./routes/api/images")
+// Allows us to parse the json sent to the front end.
+const bodyParser = require("body-parser");
+// Verifies incoming request tokens to project routes.
+const passport = require("passport");
+require("./config/passport")(passport);
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Hello World"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
+// Sets up connection to MondoDB.
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+// Tells the server which potrt to run on.
 const port = process.env.PORT || 5000;
 
+// Sets up a basic route so that we can render some information on our page.
+app.get("/", (req, res) => res.send("Coco Puffs"));
+
+// Sets passport as the bouncer.
+app.use(passport.initialize());
+
+// Parse application/x-www-form-urlencoded.
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit: 100000 }));
+// Parse application/json.
+app.use(bodyParser.json({ limit: '50mb', extended: false, parameterLimit: 100000 }));
+
+// We must tell Express to use imported routes.
+// app.use("/api/users", users);
+// app.use("/api/pins", pins);
+// app.use("/api/boards", boards);
+// app.use("/api/images", images)
+
+// Tells Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Tells our server to load the static build folder in production.
+
