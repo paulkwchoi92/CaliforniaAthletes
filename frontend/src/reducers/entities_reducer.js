9c4bd7549ba527fddf3635b5420f48088c04648d
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-10-27 17:10:30 -0700

    add reducers

---
+++ frontend/src/reducers/entities_reducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+// import interests from "./interests_reducer";
+import users from "./users_reducer";
+// import pins from "./pins_reducer";
+// import boards from "./boards_reducer";
+// import images from "./images_reducer";
+
+export default combineReducers({
+  interests,
+  users,
+  pins,
+  boards,
+  images
+});
